6d160b496496c4bd33fac3da1bb6fae6e8790c76
= <=>
2021-01-10 13:49:26 +0100

    edited Board add_ship method and helper methods, added class Field. return statements incomplete.

--- game_classes.py
+++ game_classes.py
@@ -3,43 +3,6 @@ from enumerators import *
 import random
 
 
-
-class Board:
-
-    def __init__(self):
-        self.layout = [[]]
-
-    def get_field(self, x, y):  # TODO use this
-        return self.layout[y][x]
-
-    def set_field(self, x, y, value):  # TODO use this
-        self.layout[y][x] = value
-
-    def return_board_state(self):
-        pass
-
-class EnemyBoard(Board):
-    def __init__(self):
-        super().__init__()
-        self.layout = [[BoardFieldType.UNKNOWN for x in range(10)] for y in range(10)]
-
-
-
-class PlayerBoard(Board):
-    def __init__(self):
-        super().__init__()
-        self.layout = [[BoardFieldType.WATER for x in range(10)] for y in range(10)]
-
-    def add_ship(self, ship):
-        pass
-
-    def ship_crosses_ship(self):
-        pass  # TODO
-
-    def ship_crosses_edge(self):
-        pass  # TODO
-
-
 class Ship:
     def __init__(self, x, y, size, vertical):
         self.x = x
@@ -50,11 +13,82 @@ class Ship:
     def take_hit(self):
         pass
 
-    def sink(self):
-        pass
-
     def place_ship(self, board):
         pass
+
+
+class Field:
+    def __init__(self, x, y, val):
+        self.x = x
+        self.y = y
+        self.value = val
+
+    def set_value(self, val):
+        self.value = val
+
+    def get_value(self):
+        return self.value
+
+
+class Board:
+
+    def __init__(self):
+        self.layout = [[]]
+        self.state = [[]]
+
+    def return_board_state(self):
+        return self.layout
+
+
+class EnemyBoard(Board):
+    def __init__(self):
+        super().__init__()
+        # self.layout = [[BoardFieldType.UNKNOWN for x in range(10)] for y in range(10)]
+        self.layout = [[Field(x, y, BoardFieldType.UNKNOWN) for x in range(10)] for y in range(10)]
+
+
+class PlayerBoard(Board):
+    def __init__(self):
+        super().__init__()
+        self.layout = [[Field(x, y, BoardFieldType.WATER) for x in range(10)] for y in range(10)]
+        self.state = self.layout.copy()
+
+    def add_ship(self, ship):
+        if not self.ship_crosses_ship(ship) and not self.ship_crosses_edge(ship):
+            if not ship.vertical:
+                for f in self.state[ship.y]:
+                    if ship.x <= f.x < ship.x + ship.size:
+                        f.set_value(BoardFieldType.SHIP)
+            else:
+                for f in self.state:
+                    f[ship.x].set_value(BoardFieldType.SHIP)
+        else:
+            raise RuntimeError()  # co chceme vract při failu?
+        return ship  # message nebo messagecommand
+
+    def ship_crosses_ship(self, ship):
+        if not ship.vertical:
+            for f in self.state[ship.y]:
+                if ship.x <= f.x < ship.x + ship.size:
+                    if f.value == BoardFieldType.SHIP:
+                        return True
+        else:
+            for row in self.state:
+                for f in row[ship.x:ship.x + ship.size]:
+                    if f.value == BoardFieldType.SHIP:
+                        return True
+        return False
+
+    def ship_crosses_edge(self, ship: Ship):
+        if not ship.vertical:
+            if (ship.x + ship.size) > 9:
+                return True
+            return False
+        else:
+            if (ship.x + ship.size) > 9:
+                return True
+            return False
+
 
 class Player:
     def __init__(self, nick, salt):
@@ -63,7 +97,7 @@ class Player:
         self.enemy_board = None
         self.salt = salt
         self.ships = []
-    
+
     def add_ship_to_board(self, x, y, size, vertical):
         ship = Ship(x, y, size, vertical)
         self.ships.append(ship)
@@ -88,12 +122,12 @@ class Player:
         return
 
     def shoot(self, x, y, game_id):
-        pass    
+        pass
 
 
 class Game():
     def __init__(self, host):
-        self.id = random.randint(0,9999999999999)
+        self.id = random.randint(0, 9999999999999)
         self.state = GameState.WAITING  # active / waiting
         self.hit_stat = {PlayerType.HOST: 0, PlayerType.PEER: 0}
         self.player_nicks = {PlayerType.HOST: host.nick, PlayerType.PEER: None}
@@ -124,7 +158,7 @@ class Game():
         pass
 
     def round(self, x, y):
-        pass # check if correct player calls round if not, send error
+        pass  # check if correct player calls round if not, send error
 
     def join_peer(self, peer, _hash):
         if self.player_nicks[PlayerType.PEER] is not None:
